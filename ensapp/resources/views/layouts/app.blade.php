d119d024304ac799c2bc47aafd57e7178e606829
AbdallahIsmaili <aismaili691@gmail.com>
2024-01-04 08:38:47 -0800

    Update app.blade.php

--- ensapp/resources/views/layouts/app.blade.php
+++ ensapp/resources/views/layouts/app.blade.php
@@ -48,6 +48,13 @@
               <span class="">Modules</span>
             </a>
           </li>
+          
+          <li class="cursor-pointer my-1 py-1 px-6 hover:bg-orange-500 mx-6 rounded-lg transition ease-in-out duration-500">
+            <a href="" class="font-semibold flex items-center justify-start">
+              <i class="bx bx-book text-2xl mr-2 "></i>
+              <span class="">calendar</span>
+            </a>
+          </li>
 
         </ul>
 
